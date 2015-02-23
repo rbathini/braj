2cfca50b41e0a0bbb61c5d85b525493a9371cfd4
rbathini <brajesh.mgl@gmail.com>
2015-02-23 13:19:39 -0800

    Added project to the git
    Added project to the git

---
+++ VideoLibrary/VideoLibrary/VideoLibrary/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Youtube()
+        {
+            return View();
+        }
+
+        public ActionResult PlayVideo()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Comedy()
+        {
+            return View();
+        }
+
+        public ActionResult ShortFilms()
+        {
+            return View();
+        }
+
+        public ActionResult Music()
+        {
+            return View();
+        }
+
+        public ActionResult Movies()
+        {
+            return View();
+        }
+
+        public ActionResult Devotional()
+        {
+            return View();
+        }
+
+        public ActionResult Entertainment()
+        {
+            return View();
+        }
+
+        public ActionResult VideoSongs()
+        {
+            return View();
+        }
+
+
+        public ActionResult TVChannels()
+        {
+            return View();
+        }
+
+        public ActionResult Heros()
+        {
+            return View();
+        }
+    }
+}
---
+++ VideoLibrary/VideoLibrary/VideoLibrary/Controllers/TVProgramsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoLibrary.Controllers
+{
+    public class TVProgramsController : Controller
+    {
+        //
+        // GET: /TVPrograms/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Jabardasth()
+        {
+            return View();
+        }
+    }
+}
